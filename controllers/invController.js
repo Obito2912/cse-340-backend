8b922f1be1faae793236dd02d23679dfb511b6c4
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-04-03 20:55:35 -0600

    Get as close as possible to finish the learning activities. Implement the update vehicle view

--- controllers/invController.js
+++ controllers/invController.js
@@ -41,9 +41,12 @@ invCont.buildByInvId = async function(req, res, next) {
  * ************************** */
 invCont.buildManagementView = async function(req, res, next) {
   let nav = await utilities.getNav();
+  const classificationSelect = await utilities.buildClassificationList()
+
   res.render("./inventory/management", {
     title: "Inventory Management",
     nav,
+    classificationSelect,
     notice: req.flash("notice")
   });
 };
@@ -99,6 +102,57 @@ invCont.buildAddInventory = async function (req, res) {
   })
 }
 
+// GET route for Inventory Edit View
+invCont.buildEditInventoryView = async function(req, res, next) {
+  let nav = await utilities.getNav();
+  const invId = req.params.invId;
+  // Retrieve the inventory item data using its id
+  const itemData = await invModel.getInventoryItemById(invId);
+  if (!itemData) {
+    req.flash("notice", "Item not found");
+    return res.redirect("/inv/");
+  }
+
+  const classificationList = await utilities.buildClassificationList(itemData.classification_id);
+  // Render the edit view, passing along the item data and navigation
+  res.render("./inventory/edit-inventory", {
+    title: `Edit ${itemData.inv_make} ${itemData.inv_model}`,
+    nav,
+    classificationList,
+    errors: null,
+    item: itemData,
+    notice: req.flash("notice")
+  });
+};
+
+/* ***************************
+ *  Build edit inventory view
+ * ************************** */
+invCont.editInventoryView = async function (req, res, next) {
+  const invId = parseInt(req.params.invId)
+  let nav = await utilities.getNav()
+  const itemData = await invModel.getInventoryItemById(invId)
+  const classificationSelect = await utilities.buildClassificationList(itemData.classification_id)
+  const itemName = `${itemData.inv_make} ${itemData.inv_model}`
+  res.render("./inventory/edit-inventory", {
+    title: "Edit " + itemName,
+    nav,
+    classificationSelect: classificationSelect,
+    errors: null,
+    invId: itemData.invId,
+    inv_make: itemData.inv_make,
+    inv_model: itemData.inv_model,
+    inv_year: itemData.inv_year,
+    inv_description: itemData.inv_description,
+    inv_image: itemData.inv_image,
+    inv_thumbnail: itemData.inv_thumbnail,
+    inv_price: itemData.inv_price,
+    inv_miles: itemData.inv_miles,
+    inv_color: itemData.inv_color,
+    classification_id: itemData.classification_id
+  })
+}
+
 // Handle form POST
 invCont.addInventory = async function (req, res) {
   let nav = await utilities.getNav()
@@ -147,6 +201,85 @@ invCont.addInventory = async function (req, res) {
       notice: req.flash("notice"),
       ...req.body
     })
+  }
+}
+
+/* ***************************
+ *  Update Inventory Data
+ * ************************** */
+invCont.updateInventory = async function (req, res, next) {
+  let nav = await utilities.getNav();
+
+  // Convert numeric fields explicitly
+  const inv_id = parseInt(req.body.inv_id, 10);
+  const inv_year = parseInt(req.body.inv_year, 10);
+  const inv_miles = parseInt(req.body.inv_miles, 10);
+  const classification_id = parseInt(req.body.classification_id, 10);
+  const inv_price = parseFloat(req.body.inv_price); // if price is a float
+
+  // Get the rest of the fields
+  const {
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_color
+  } = req.body;
+
+  const updateResult = await invModel.updateInventory(
+    inv_id,  
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id
+  );
+
+  if (updateResult) {
+    const itemName = updateResult.inv_make + " " + updateResult.inv_model;
+    req.flash("notice", `The ${itemName} was successfully updated.`);
+    res.redirect("/inv/");
+  } else {
+    const classificationList = await utilities.buildClassificationList(classification_id);
+    const itemName = `${inv_make} ${inv_model}`;
+    req.flash("notice", "Sorry, the update failed.");
+    res.status(501).render("inventory/edit-inventory", {
+      title: "Edit " + itemName,
+      nav,
+      classificationList,
+      errors: null,
+      inv_id,
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id,
+      notice: req.flash("notice")
+    });
+  }
+};
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
   }
 }
 
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -30,15 +30,35 @@ router.post(
 
 // Show the form
 router.get(
-    "/add-inventory",
-    utilities.handleErrors(invController.buildAddInventory)
-  )
-  
-  // Handle the form submission
-  router.post(
-    "/add-inventory",
-    validate.inventoryRules(),
-    validate.checkInventoryData,
-    utilities.handleErrors(invController.addInventory)
-  )
+  "/add-inventory",
+  utilities.handleErrors(invController.buildAddInventory)
+);
+
+// Handle the form submission
+router.post(
+  "/add-inventory",
+  validate.inventoryRules(),
+  validate.checkInventoryData,
+  utilities.handleErrors(invController.addInventory)
+);
+
+router.get(
+  "/getInventory/:classification_id",
+  utilities.handleErrors(invController.getInventoryJSON)
+);
+
+// GET route to present the inventory edit view for a specific item
+router.get("/edit/:invId", utilities.handleErrors(invController.buildEditInventoryView));
+
+// POST route to update an existing inventory item.
+// This route uses newInventoryRules() and checkUpdateData middleware so that 
+// the update data meets the same validation requirements as the add process,
+// and in case of errors, redirects back to the edit view.
+router.post(
+  "/edit-inventory",
+  validate.newInventoryRules(),
+  validate.checkUpdateData,
+  utilities.handleErrors(invController.updateInventory)
+);
+
 module.exports = router;
